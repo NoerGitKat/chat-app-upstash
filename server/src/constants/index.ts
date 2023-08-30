bea2896773df573e68392b323b7a541669010f68
Noer Paanakker <noerpaanakker@gmail.com>
2023-08-30 22:00:29 +0200

    Added new messages

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -8,3 +8,5 @@ export const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3001";
 export const UPSTASH_REDIS_REST_URL = process.env.UPSTASH_REDIS_REST_URL;
 export const CONNECTION_COUNT_KEY = "chat:connection-count";
 export const CONNECTION_COUNT_UPDATED_CHANNEL = "chat:connection-count-updated";
+export const NEW_MESSAGE_CHANNEL = "chat:new-message";
+export const ALL_MESSAGES_CHANNEL = "chat:messages";
--- server/src/sockets/index.ts
+++ server/src/sockets/index.ts
@@ -1,6 +1,13 @@
 import { FastifyInstance } from "fastify";
 import { publisher, subscriber } from "../cache";
-import { CONNECTION_COUNT_KEY, CONNECTION_COUNT_UPDATED_CHANNEL } from "../constants";
+import {
+  CONNECTION_COUNT_KEY,
+  CONNECTION_COUNT_UPDATED_CHANNEL,
+  NEW_MESSAGE_CHANNEL,
+  ALL_MESSAGES_CHANNEL,
+  PORT
+} from "../constants";
+import { randomUUID } from "crypto";
 
 let connectedClients = 0;
 
@@ -8,17 +15,21 @@ export async function startSockets(app: FastifyInstance) {
   const currentChannelCount = await publisher.get(CONNECTION_COUNT_KEY);
   if (!currentChannelCount) await publisher.set(CONNECTION_COUNT_KEY, 0);
 
-  // Socket IO
   app.io.on("connection", async (io) => {
     console.log("Client is connected!");
     const newConnection = await publisher.incr(CONNECTION_COUNT_KEY);
     connectedClients++;
     await publisher.publish(CONNECTION_COUNT_UPDATED_CHANNEL, String(newConnection));
 
+    io.on(NEW_MESSAGE_CHANNEL, async ({ message }) => {
+      if (!message) return;
+      await publisher.publish(NEW_MESSAGE_CHANNEL, message);
+    });
+
     io.on("disconnect", async () => {
       connectedClients--;
       const updatedCount = await publisher.decr(CONNECTION_COUNT_KEY);
-      console.log("updatedCount", updatedCount);
+
       await publisher.publish(CONNECTION_COUNT_UPDATED_CHANNEL, String(updatedCount));
       console.log("Disconnected.");
     });
@@ -34,13 +45,32 @@ export async function startSockets(app: FastifyInstance) {
     );
   });
 
-  subscriber.on("message", (channel, text) => {
-    if (channel === CONNECTION_COUNT_UPDATED_CHANNEL) {
-      app.io.emit(CONNECTION_COUNT_UPDATED_CHANNEL, {
-        count: text
-      });
+  subscriber.subscribe(NEW_MESSAGE_CHANNEL, (err, count) => {
+    if (err) return console.error(err);
 
-      return;
+    console.log(
+      `${count} client${count !== 1 ? "s" : ""} connected to ${NEW_MESSAGE_CHANNEL} channel`
+    );
+  });
+
+  subscriber.on("message", (channel, text) => {
+    switch (channel) {
+      case CONNECTION_COUNT_UPDATED_CHANNEL:
+        app.io.emit(CONNECTION_COUNT_UPDATED_CHANNEL, {
+          count: text
+        });
+        break;
+      case NEW_MESSAGE_CHANNEL:
+        app.io.emit(NEW_MESSAGE_CHANNEL, {
+          message: text,
+          id: randomUUID(),
+          createdAt: new Date(),
+          port: PORT
+        });
+        break;
+
+      default:
+        return;
     }
   });
 }
