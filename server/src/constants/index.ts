eaa667979c030973d8a931d906e7b2e8adea5f9f
Noer Paanakker <noerpaanakker@gmail.com>
2023-08-31 18:08:56 +0200

    Containerized server

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -6,6 +6,7 @@ export const PORT = parseInt(process.env.PORT || "3001");
 export const HOST = process.env.HOST || "0.0.0.0";
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || "http://localhost:3001";
 export const UPSTASH_REDIS_REST_URL = process.env.UPSTASH_REDIS_REST_URL;
+
 export const CONNECTION_COUNT_KEY = "chat:connection-count";
 export const CONNECTION_COUNT_UPDATED_CHANNEL = "chat:connection-count-updated";
 export const NEW_MESSAGE_CHANNEL = "chat:new-message";
